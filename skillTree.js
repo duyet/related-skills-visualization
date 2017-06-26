a46174b9e57415bdd88a149d34d8f7fb410c0f33
Van-Duyet Le <me@duyet.net>
2017-06-26 18:32:45 +0700

    Update

---
+++ skillTree.js
@@ -0,0 +1,111 @@
+var skillTree = {
+  "name": "Apache Tomcat",
+  "data": {"score":0.7},
+  "children": [
+    {
+      "name": "Eclipse",
+  "data": {"score":0.7},
+  "children": [
+        {
+          "name": "NetBeans"
+        },
+        {
+          "name": "GlassFish"
+        },
+        {
+          "name": "JPA"
+        },
+        {
+          "name": "JSP"
+        },
+        {
+          "name": "Apache Struts"
+        }
+      ]
+    },
+    {
+      "name": "Apache Maven",
+  "data": {"score":0.7},
+  "children": [
+        {
+          "name": "Apache Ant"
+        },
+        {
+          "name": "Jenkins"
+        },
+        {
+          "name": "Hudson"
+        },
+        {
+          "name": "SVN"
+        },
+        {
+          "name": "Apache Subversion"
+        }
+      ]
+    },
+    {
+      "name": "Apache HTTP Server",
+  "data": {"score":0.7},
+  "children": [
+        {
+          "name": "Web server"
+        },
+        {
+          "name": "WildFly"
+        },
+        {
+          "name": "Red Hat Enterprise Linux"
+        },
+        {
+          "name": "Nginx"
+        },
+        {
+          "name": "MySQL"
+        }
+      ]
+    },
+    {
+      "name": "JUnit",
+  "data": {"score":0.7},
+  "children": [
+        {
+          "name": "Spring MVC"
+        },
+        {
+          "name": "TestNG"
+        },
+        {
+          "name": "Mockito"
+        },
+        {
+          "name": "YUI"
+        },
+        {
+          "name": "Selenium"
+        }
+      ]
+    },
+    {
+      "name": "Application server",
+  "data": {"score":0.7},
+  "children": [
+        {
+          "name": "IBM WebSphere"
+        },
+        {
+          "name": "IBM WebSphere Application Server"
+        },
+        {
+          "name": "IBM WebSphere administration"
+        },
+        {
+          "name": "IBM HTTP Server"
+        },
+        {
+          "name": "Web applications"
+        }
+      ]
+    }
+  ]
+}
